dd027eb3b39abdf099d8c493299d96f3e56a1ec6
Shreyas Krishnakumar <shreyas@tarides.com>
2023-05-24 14:04:08 +0530

    simplify match expression

--- lib/parser.ml
+++ lib/parser.ml
@@ -7,25 +7,23 @@ let extract_links text =
     | Url (href, s, _) :: tl ->
         let lst = href :: loop s in
         lst @ loop tl
-    | Blockquote hd :: tl
-    | Paragraph hd :: tl
-    | Emph hd :: tl
-    | Bold hd :: tl
-    | H1 hd :: tl
-    | H2 hd :: tl
-    | H3 hd :: tl
-    | H4 hd :: tl
-    | H5 hd :: tl
-    | H6 hd :: tl ->
+    | ( Blockquote hd
+      | Paragraph hd
+      | Emph hd
+      | Bold hd
+      | H1 hd
+      | H2 hd
+      | H3 hd
+      | H4 hd
+      | H5 hd
+      | H6 hd )
+      :: tl ->
         let lst = loop hd in
         lst @ loop tl
-    | (Ul l | Ol l) :: tl | (Ulp l | Olp l) :: tl ->
+    | (Ul l | Ol l | Ulp l | Olp l) :: tl ->
         let lst = List.concat_map loop l in
         lst @ loop tl
-    | Html (_, _, body) :: tl ->
-        let lst = loop body in
-        lst @ loop tl
-    | Html_block (_, _, body) :: tl ->
+    | (Html (_, _, body) | Html_block (_, _, body)) :: tl ->
         let lst = loop body in
         lst @ loop tl
     | _ :: tl -> loop tl
