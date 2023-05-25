2f1f12e9ade83c137968d7b00348dad70f42e9e9
Shreyas K <63252528+Shreyas-21@users.noreply.github.com>
2023-05-25 15:49:00 +0530

    get link status (#7)
    * get link status
    
    * simplify pretty print loop
    
    * have a single loop for pretty_print
    
    ---------
    
    Co-authored-by: Shreyas Krishnakumar <shreyas@tarides.com>

--- bin/main.ml
+++ bin/main.ml
@@ -2,8 +2,17 @@ open Olinkcheck
 
 let file_contents file = In_channel.(with_open_bin file In_channel.input_all)
 
+let pretty_print_link_status (link, (status_code, status_string)) =
+  Printf.printf "%s - %i %s\n" link status_code status_string
+
+let with_arg f x = (x, f x)
+
 let () =
   if Array.length Sys.argv < 2 then
     print_endline "Usage: olinkcheck filename.md"
   else
-    List.iter print_endline (Parser.extract_links (file_contents Sys.argv.(1)))
+    let links = Parser.extract_links (file_contents Sys.argv.(1)) in
+    List.iter
+      (fun link ->
+        link |> with_arg Link.get_link_status |> pretty_print_link_status)
+      links
--- test/olinkcheck.ml
+++ test/olinkcheck.ml
@@ -24,13 +24,33 @@ let test_text_with_links () =
         [link2](https://link2.com) - [link3](http://link3.com)  \
         ###[link4](http://link4.com)")
 
+let test_valid_link () =
+  Alcotest.(check (pair int string))
+    "same pair" (200, "OK")
+    (Link.get_link_status "http://www.google.com")
+
+let test_nonexistent_link () =
+  Alcotest.(check (pair int string))
+    "same pair" (404, "Not Found")
+    (Link.get_link_status "http://google.com/does-not-exist")
+
+let test_invalid_link () =
+  Alcotest.(check (pair int string))
+    "same pair" (0, "Invalid Link") (Link.get_link_status "")
+
 let () =
-  Alcotest.run "Extract Links"
+  Alcotest.run "Olinkcheck"
     [
       ( "extract-links",
         [
           Alcotest.test_case "Empty text" `Quick test_empty_text;
           Alcotest.test_case "Text without links" `Quick test_text_without_links;
           Alcotest.test_case "Text with links" `Slow test_text_with_links;
+        ] );
+      ( "link-status",
+        [
+          Alcotest.test_case "Valid link" `Quick test_valid_link;
+          Alcotest.test_case "Invalid link" `Quick test_invalid_link;
+          Alcotest.test_case "Non-existent link" `Quick test_nonexistent_link;
         ] );
     ]
