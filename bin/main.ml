356d9c855c01a7ecfca3d82addbc71e166c846f9
Shreyas Krishnakumar <shreyas@tarides.com>
2023-05-25 15:37:04 +0530

    have a single loop for pretty_print

--- bin/main.ml
+++ bin/main.ml
@@ -5,11 +5,14 @@ let file_contents file = In_channel.(with_open_bin file In_channel.input_all)
 let pretty_print_link_status (link, (status_code, status_string)) =
   Printf.printf "%s - %i %s\n" link status_code status_string
 
+let with_arg f x = (x, f x)
+
 let () =
   if Array.length Sys.argv < 2 then
     print_endline "Usage: olinkcheck filename.md"
   else
     let links = Parser.extract_links (file_contents Sys.argv.(1)) in
-    List.map Link.get_link_status links
-    |> List.combine links
-    |> List.iter pretty_print_link_status
+    List.iter
+      (fun link ->
+        link |> with_arg Link.get_link_status |> pretty_print_link_status)
+      links
