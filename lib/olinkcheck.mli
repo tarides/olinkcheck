b93b657b168e8300fe305d152a832cae1381d42f
Shreyas Krishnakumar <shreyas@tarides.com>
2023-05-30 13:42:06 +0530

    use module type of Link

--- lib/olinkcheck.mli
+++ lib/olinkcheck.mli
@@ -1,7 +1,5 @@
 val extract_links : string -> string list
 
 module Link : sig
-  val status : string -> int * string
-  val request : string -> Hyper.status Lwt.t
-  val client : ('a -> [< Hyper.status ]) -> 'a -> int * string
+  include module type of Link
 end
