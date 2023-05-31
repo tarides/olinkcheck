89b90ce201d9452919cb4f3636d8b5dd0dc35f3b
Shreyas Krishnakumar <shreyas@tarides.com>
2023-05-31 10:50:20 +0530

    move helper functions to utils

---
+++ bin/utils.ml
@@ -0,0 +1,6 @@
+let file_contents file = In_channel.(with_open_bin file In_channel.input_all)
+
+let pretty_print_link_status (link, (status_code, status_string)) =
+  Printf.printf "%s - %i %s\n" link status_code status_string
+
+let with_arg f x = (x, f x)
