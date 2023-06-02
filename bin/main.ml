2bc0d41b6bc2dd7196bb5ef39e0f773c6aee63a6
Shreyas K <63252528+Shreyas-21@users.noreply.github.com>
2023-06-02 11:35:47 +0530

    use cmdliner to process the command line (#20)
    * use cmdliner to process the command line
    
    * remove anonymous function
    
    ---------
    
    Co-authored-by: Shreyas Krishnakumar <shreyas@tarides.com>

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,31 @@
 open Olinkcheck
+open Cmdliner
 
-let () =
-  if Array.length Sys.argv < 2 then
-    print_endline "Usage: olinkcheck filename.md"
-  else
-    let links = extract_links (Utils.file_contents Sys.argv.(1)) in
-    List.iter
-      (fun link ->
-        link
-        |> (fun f x -> (x, f x)) Link.status
-        |> Utils.pretty_print_link_status)
-      links
+type format = Markdown
+
+let format =
+  let doc = "Format of the input file." in
+  let format = Arg.enum [ ("md", Markdown) ] in
+  Arg.(required & pos 0 (some format) None & info [] ~docv:"FORMAT" ~doc)
+
+let file =
+  let doc = "File to check links." in
+  Arg.(required & pos 1 (some non_dir_file) None & info [] ~docv:"FILE" ~doc)
+
+let olinkcheck format file =
+  match format with
+  | Markdown ->
+      `Ok
+        (let links = extract_links (Utils.file_contents file) in
+         List.iter
+           (fun link ->
+             (link, Link.status link) |> Utils.pretty_print_link_status)
+           links)
+
+let cmd =
+  let doc = "Check the status of links in a file." in
+  let info = Cmd.info "olinkcheck" ~doc in
+  Cmd.v info Term.(ret (const olinkcheck $ format $ file))
+
+let main = exit (Cmd.eval cmd)
+let () = main
