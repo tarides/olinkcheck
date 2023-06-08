b4ee52369a0dca611519386227ff23759c6ea09b
Shreyas Krishnakumar <shreyas@tarides.com>
2023-06-08 17:42:45 +0530

    simplify function

--- bin/utils.ml
+++ bin/utils.ml
@@ -3,25 +3,17 @@ let file_contents file = In_channel.(with_open_bin file In_channel.input_all)
 let pretty_print_link_status (link, (status_code, status_string)) =
   Printf.printf "%s - %i %s\n" link status_code status_string
 
+let filter_with_ext ext = List.filter (fun file -> Filename.extension file = ext)
+
 let rec files_with_ext ext file =
   if Sys.file_exists file then (
     try
       if Sys.is_directory file then
-        let files =
-          file |> Sys.readdir |> Array.to_list
-          |> List.map (Filename.concat file)
-        in
-        let files_in_dir =
-          List.filter
-            (fun file ->
-              (not (Sys.is_directory file)) && Filename.extension file = ext)
-            files
-        in
-        let files_in_child_dirs =
-          List.concat_map (files_with_ext ext)
-            (List.filter Sys.is_directory files)
-        in
-        files_in_dir @ files_in_child_dirs
+        file |> Sys.readdir |> Array.to_list
+        |> List.map (Filename.concat file)
+        |> List.partition Sys.is_directory
+        |> fun (dirs, non_dirs) ->
+        filter_with_ext ext non_dirs @ List.concat_map (files_with_ext ext) dirs
       else if Filename.extension file = ext then [ file ]
       else []
     with Sys_error x ->
