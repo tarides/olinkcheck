be3096cbbeae37dd354888d0517ce4e677b3c33a
Shreyas Krishnakumar <shreyas@tarides.com>
2023-06-08 11:41:12 +0530

    add block and block_list

--- lib/markdown.ml
+++ lib/markdown.ml
@@ -29,6 +29,13 @@ let extract_links md =
   in
   loop md
 
+let rec loop f v i xs =
+  match (i, xs) with
+  | i, [] -> (i, [])
+  | i, x :: xs ->
+      let i', x' = f v i x in
+      (i', x' :: snd (loop f v i' xs))
+
 let rec replace_in_inline v i u =
   match (u, v) with
   | Link (a, { label; destination; title }), (n, new_dest) ->
@@ -44,13 +51,60 @@ let rec replace_in_inline v i u =
       let i', x' = replace_in_inline v i x in
       (i', Strong (a, x'))
   | Concat (a, xs), v ->
-      let rec loop i xs =
-        match (i, xs) with
-        | i, [] -> (i, [])
-        | i, x :: xs ->
-            let i', x' = replace_in_inline v i x in
-            (i', x' :: snd (loop i' xs))
-      in
-      let i', xs' = loop i xs in
+      let i', xs' = loop replace_in_inline v i xs in
       (i', Concat (a, xs'))
   | el, _ -> (i, el)
+
+let rec replace_in_block v i u =
+  match (u, v) with
+  | Paragraph (a, x), v ->
+      let i', x' = replace_in_inline v i x in
+      (i', Paragraph (a, x'))
+  | List (a, b, c, xss), v ->
+      let rec loop1 i xss =
+        match (i, xss) with
+        | i, [] -> (i, [])
+        | i, xs :: xss ->
+            let i', xs' = loop replace_in_block v i xs in
+            (i', [ xs' ] @ snd (loop1 i' xss))
+      in
+      let i'', xss' = loop1 i xss in
+      (i'', List (a, b, c, xss'))
+  | Blockquote (a, xs), v ->
+      let i', xs' = loop replace_in_block v i xs in
+      (i', Blockquote (a, xs'))
+  | Heading (a, b, x), v ->
+      let i', x' = replace_in_inline v i x in
+      (i', Heading (a, b, x'))
+  | Definition_list (a, dl), v ->
+      let rec loop1 i dl =
+        match (i, dl) with
+        | i, [] -> (i, [])
+        | i, { term; defs } :: dl ->
+            let i', term' = replace_in_inline v i term in
+            let i'', ds' = loop replace_in_inline v i' defs in
+            (i'', { term = term'; defs = ds' } :: snd (loop1 i dl))
+      in
+      let i', dl' = loop1 i dl in
+      (i', Definition_list (a, dl'))
+  | Table (a, headers, rows), v ->
+      let rec header_loop i hs =
+        match (i, hs) with
+        | i, [] -> (i, [])
+        | i, (hdr, al) :: hs ->
+            let i', hdr' = replace_in_inline v i hdr in
+            (i', (hdr', al) :: snd (header_loop i' hs))
+      in
+      let rec rows_loop i rows =
+        match (i, rows) with
+        | i, [] -> (i, [])
+        | i, row :: rows ->
+            let i', row' = loop replace_in_inline v i row in
+            (i', [ row' ] @ snd (rows_loop i' rows))
+      in
+      let i', hs' = header_loop i headers in
+      let i'', rows' = rows_loop i' rows in
+      (i'', Table (a, hs', rows'))
+  | el, _ -> (i, el)
+
+let replace_in_doc = loop replace_in_block
