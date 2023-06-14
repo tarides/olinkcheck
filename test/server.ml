9ddc68dba25f545fd4abdeca2eecd8170f32a0df
Shreyas Krishnakumar <shreyas@tarides.com>
2023-06-14 18:04:32 +0530

    use ezcurl instead of hyper

--- test/server.ml
+++ test/server.ml
@@ -3,7 +3,7 @@ let webserver port signal status =
   Lwt_io.establish_server_with_client_address
     Unix.(ADDR_INET (inet_addr_loopback, port))
     (fun _handle (_in, out) ->
-      let* () = Lwt_io.write out ("HTTP/1.1 " ^ status ^ "\r\n\r\n") in
+      let* () = Lwt_io.write out ("HTTP/1.1 " ^ status ^ "\r\n\r\n ") in
       let* () = Lwt_io.close out in
       Lwt.wakeup_later signal ();
       Lwt.return_unit)
