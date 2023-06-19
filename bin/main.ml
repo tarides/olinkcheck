ec614ef2db58ab5635d71c387149cc4b05ba12da
Shreyas Krishnakumar <shreyas@tarides.com>
2023-06-19 14:43:57 +0530

    add option to skip reporting OK status

--- bin/main.ml
+++ bin/main.ml
@@ -12,23 +12,27 @@ let file =
   let doc = "File to check links." in
   Arg.(required & pos 1 (some file) None & info [] ~docv:"FILE" ~doc)
 
-let olinkcheck format file =
+let skip_ok =
+  let doc = "Flag to skip reporting links with status 200 OK" in
+  Arg.(value & flag & info [ "skip-ok" ] ~doc)
+
+let olinkcheck skip_ok format file =
   match format with
   | Markdown ->
       `Ok
         Markdown.(
-          Utils.pretty_print_link_status_from_file ".md" from_string
+          Utils.pretty_print_link_status_from_file skip_ok ".md" from_string
             extract_links file)
   | Plaintext ->
       `Ok
         Plaintext.(
-          Utils.pretty_print_link_status_from_file ".txt" from_string
+          Utils.pretty_print_link_status_from_file skip_ok ".txt" from_string
             extract_links file)
 
 let cmd =
   let doc = "Check the status of links in a file." in
   let info = Cmd.info "olinkcheck" ~doc in
-  Cmd.v info Term.(ret (const olinkcheck $ format $ file))
+  Cmd.v info Term.(ret (const olinkcheck $ skip_ok $ format $ file))
 
 let main = exit (Cmd.eval cmd)
 let () = main
--- bin/utils.ml
+++ bin/utils.ml
@@ -1,7 +1,9 @@
 let file_contents file = In_channel.(with_open_bin file In_channel.input_all)
 
-let pretty_print_link_status (link, (status_code, status_string)) =
-  Printf.printf "%s - %i %s\n" link status_code status_string
+let pretty_print_link_status skip_ok (link, (status_code, status_string)) =
+  if (skip_ok && status_code <> 200) || not skip_ok then
+    Printf.printf "%s - %i %s\n" link status_code status_string
+  else ()
 
 let with_ext ext = List.filter (fun file -> Filename.extension file = ext)
 
@@ -17,7 +19,8 @@ let rec files_with_ext ext file =
   else if Filename.extension file = ext then [ file ]
   else []
 
-let pretty_print_link_status_from_file ext from_string extract_links file =
+let pretty_print_link_status_from_file skip_ok ext from_string extract_links
+    file =
   file |> files_with_ext ext
   |> List.iter (fun file ->
          try
@@ -26,5 +29,5 @@ let pretty_print_link_status_from_file ext from_string extract_links file =
                 print_endline file;
                 let statuses = Olinkcheck.Link.status_many links in
                 List.combine links statuses)
-           |> List.iter pretty_print_link_status
+           |> List.iter (pretty_print_link_status skip_ok)
          with Sys_error _ -> ())
