31aaa91bc01f57e8772c732c50c5ea1197a979f0
Shreyas Krishnakumar <shreyas@tarides.com>
2023-06-22 23:21:02 +0530

    space out requests to avoid rate limits

--- bin/utils.ml
+++ bin/utils.ml
@@ -18,6 +18,7 @@ let rec files_with_ext ext file =
   else []
 
 let pretty_print_link_status_from_file ext from_string extract_links file =
+  let start_time = ref (Unix.time ()) in
   file |> files_with_ext ext
   |> List.iter (fun file ->
          try
@@ -26,15 +27,22 @@ let pretty_print_link_status_from_file ext from_string extract_links file =
                 print_endline file;
                 let statuses = Olinkcheck.Link.status_many links in
                 List.combine links statuses)
-           |> List.iter pretty_print_link_status
+           |> List.iter pretty_print_link_status;
+           let end_time = Unix.time () in
+           if end_time -. !start_time >= 600. then Unix.sleep 60;
+           start_time := Unix.time ()
          with Sys_error _ -> ())
 
 let annotate_in_file ext annotate_in_str file =
+  let start_time = ref (Unix.time ()) in
   file |> files_with_ext ext
   |> List.iter (fun file ->
          try
            file |> file_contents |> annotate_in_str |> fun new_str ->
            let out_chan = open_out file in
            Printf.fprintf out_chan "%s" new_str;
-           close_out out_chan
+           close_out out_chan;
+           let end_time = Unix.time () in
+           if end_time -. !start_time >= 600. then Unix.sleep 60;
+           start_time := Unix.time ()
          with Sys_error _ -> ())
