6702d48d2e6922c84420b5faa2f5f5ee0f32f38e
Shreyas Krishnakumar <shreyas@tarides.com>
2023-06-23 11:40:26 +0530

    invert flag to verbose

--- bin/utils.ml
+++ bin/utils.ml
@@ -1,7 +1,7 @@
 let file_contents file = In_channel.(with_open_bin file In_channel.input_all)
 
-let pretty_print_link_status skip_ok (link, (status_code, status_string)) =
-  if (skip_ok && status_code <> 200) || not skip_ok then
+let pretty_print_link_status verbose (link, (status_code, status_string)) =
+  if (not (verbose || status_code = 200)) || verbose then
     Printf.printf "%s - %i %s\n" link status_code status_string
   else ()
 
@@ -19,7 +19,7 @@ let rec files_with_ext ext file =
   else if Filename.extension file = ext then [ file ]
   else []
 
-let pretty_print_link_status_from_file skip_ok ext from_string extract_links
+let pretty_print_link_status_from_file verbose ext from_string extract_links
     file =
   file |> files_with_ext ext
   |> List.iter (fun file ->
@@ -29,5 +29,5 @@ let pretty_print_link_status_from_file skip_ok ext from_string extract_links
                 print_endline file;
                 let statuses = Olinkcheck.Link.status_many links in
                 List.combine links statuses)
-           |> List.iter (pretty_print_link_status skip_ok)
+           |> List.iter (pretty_print_link_status verbose)
          with Sys_error _ -> ())
